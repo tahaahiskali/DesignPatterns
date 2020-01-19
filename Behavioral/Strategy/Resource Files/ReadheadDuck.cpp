1805137d9b93c221a347a6c7551fb3dc0617c123
Taha Ahıskalı <tahaahiskali@gmail.com>
2020-01-19 21:07:47 +0300

    Derived Class from ReadheadDuck

---
+++ Behavioral/Strategy/Resource Files/ReadheadDuck.cpp
@@ -0,0 +1,13 @@
+#include "ReadheadDuck.hpp"
+
+#include <iostream>
+
+ReadheadDuck::ReadheadDuck()
+{
+
+}
+
+void ReadheadDuck::display()
+{
+    std::cout << "ReadheadDuck display" << std::endl;
+}
