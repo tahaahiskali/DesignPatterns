057ec3939632d26be75bd50f6d506c8490fd3a51
Taha Ahıskalı <tahaahiskali@gmail.com>
2020-01-19 21:04:10 +0300

    Derived Class from Duck

---
+++ Behavioral/Strategy/Header Files/ReadheadDuck.hpp
@@ -0,0 +1,16 @@
+#ifndef READHEAD_DUCK_H
+#define READHEAD_DUCK_H
+
+#include "Duck.hpp"
+
+class ReadheadDuck {
+
+    public:
+
+        ReadheadDuck();
+
+        void display();
+
+};
+
+#endif//READHEAD_DUCK_H
