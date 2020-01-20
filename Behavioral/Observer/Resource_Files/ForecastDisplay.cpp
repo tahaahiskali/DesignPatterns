6aac2019581f11c57ddc736ca259acf44626537f
Taha Ahıskalı <tahaahiskali@gmail.com>
2020-01-20 20:37:53 +0300

    Implementation of ForecastDisplay.hpp

---
+++ Behavioral/Observer/Resource_Files/ForecastDisplay.cpp
@@ -0,0 +1,33 @@
+#include "../Header_Files/ForecastDisplay.hpp"
+
+#include <iostream>
+
+
+void ForecastDisplay::update( float temperature, float humidity, float pressure )
+{
+
+    (void) humidity;
+    (void) pressure;
+       
+    lastTemperature  = currentTemperature;
+    currentTemperature = temperature;
+
+    display();
+}
+
+void ForecastDisplay::display() {
+
+    if ( currentTemperature > lastTemperature ) {
+        std::cout << "Weather forecast: will be hotter  \n";
+    } else if ( currentTemperature < lastTemperature ) {
+        std::cout << "Weather forecast: will be colder  \n";
+    } else {
+        std::cout << "Weather forecast: will be similar \n";
+    }
+
+}
+
+ForecastDisplay::ForecastDisplay( ISubject &inWeatherData) : lastTemperature(0), currentTemperature(0), weatherData(inWeatherData)
+{
+    this->weatherData.registerObserver( this );
+}
