c7b84580fe6edf06f34a943a86415ba74d10b573
Taha Ahıskalı <taha.ahiskali@tengen.com.tr>
2020-02-05 17:39:52 +0300

    Implementation of FlyWithWings.hpp

---
+++ Behavioral/Strategy/FlyWithWings.cpp
@@ -0,0 +1,18 @@
+#include "FlyWithWings.hpp"
+
+#include <iostream>
+
+FlyWithWings::FlyWithWings()
+{
+
+}
+
+FlyWithWings::~FlyWithWings()
+{
+
+}
+
+void FlyWithWings::fly()
+{
+    std::cout << " I'm flying! " << std::endl;
+}
