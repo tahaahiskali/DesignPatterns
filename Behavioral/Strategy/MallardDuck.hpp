2b0be550a6d26e30dcc04e0fe41e332512d702fe
Taha Ahıskalı <taha.ahiskali@tengen.com.tr>
2020-02-05 17:21:51 +0300

    Derived class from Duck

---
+++ Behavioral/Strategy/MallardDuck.hpp
@@ -0,0 +1,18 @@
+#ifndef MALLARD_DUCK_HPP
+#define MALLARD_DUCK_HPP
+
+#include "Duck.hpp"
+
+class MallardDuck : public Duck {
+
+    public:
+
+        MallardDuck();
+        ~MallardDuck();
+
+        virtual void display();
+
+};
+
+
+#endif // MALLARD_DUCK_HPP
