db3b2cef20659082b2151338b4cb821979e2b5c4
Taha Ahıskalı <taha.ahiskali@tengen.com.tr>
2020-02-05 17:39:41 +0300

    Implementation of FlyNoWay.hpp

---
+++ Behavioral/Strategy/FlyNoWay.cpp
@@ -0,0 +1,18 @@
+#include "FlyNoWay.hpp"
+
+#include <iostream>
+
+FlyNoWay::FlyNoWay()
+{
+
+}
+
+FlyNoWay::~FlyNoWay()
+{
+
+}
+
+void FlyNoWay::fly()
+{
+    std::cout << " I can't fly "<<std::endl;
+}
