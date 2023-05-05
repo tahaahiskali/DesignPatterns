e6f1facee13702b0d622c960c5ccf29662c58ec7
tahaahiskali <tahaahiskali@gmail.com>
2023-05-05 15:01:05 +0300

    make interface class scroll and window

---
+++ Creational/AbstractFactory/IScrollBar.h
@@ -0,0 +1,11 @@
+#ifndef SCROLLBAR_H
+#define SCROLLBAR_H
+
+
+class IScrollBar
+{
+public:
+    virtual void setMax( int max ) = 0;
+};
+
+#endif // SCROLLBAR_H
---
+++ Creational/AbstractFactory/IWindow.h
@@ -0,0 +1,12 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+
+class IWindow
+{
+public:
+    
+    virtual void repaint() = 0;
+};
+
+#endif // WINDOW_H
